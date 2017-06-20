3caadf8b9c937dd53b1cb4f4a8370a28aa753e51
Onur Aslan <onuraslan@gmail.com>
2017-06-20 12:29:49 +0300

    Fix example

--- examples/hello.rs
+++ examples/hello.rs
@@ -3,6 +3,6 @@ extern crate i3blocks;
 
 fn main() {
     i3blocks::Script::new().on_refresh(|instance| {
-        format!("Hello World: {:?}", instance)
+        Some(format!("Hello World: {:?}", instance))
     });
 }
